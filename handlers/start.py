ebad5fd5b0ae27e54912183a3ec76cd845cbdd22
Duckoi1707 <91252600+Duckoi1707@users.noreply.github.com>
2021-09-23 16:44:58 +0700

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -160,7 +160,7 @@ async def ping_pong(client: Client, message: Message):
     m_reply = await message.reply_text("pinging...")
     delta_ping = time() - start
     await m_reply.edit_text(
-        "🏓 `PONG!!`\n"
+        "🏓 `Trạng Thái Ping!!`\n"
         f"⚡️ `{delta_ping * 1000:.3f} ms`"
     )
 
