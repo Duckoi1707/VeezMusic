0415ec05229771c3be063932cd220a3b124e4fec
Duckoi1707 <91252600+Duckoi1707@users.noreply.github.com>
2021-09-23 16:34:24 +0700

    Update audioplay.py

--- handlers/audioplay.py
+++ handlers/audioplay.py
@@ -67,7 +67,7 @@ async def stream(_, message: Message):
         position = await queues.put(message.chat.id, file=file_path)
         await message.reply_photo(
             photo=f"{QUE_IMG}",
-            caption=f"💡 **Track added to queue »** `{position}`\n\n🏷 **Name:** {title[:50]}\n⏱ **Duration:** `{duration}`\n🎧 **Request by:** {costumer}",
+            caption=f"💡 **Hàng Chờ Bài Hát »** `{position}`\n\n🏷 **Tên Bài Hát:** {title[:50]}\n⏱ **Thời Lượng:** `{duration}`\n🎧 **Khởi Chạy Bởi:** {costumer}",
             reply_markup=keyboard,
         )
         return await lel.delete()
@@ -75,8 +75,8 @@ async def stream(_, message: Message):
         callsmusic.pytgcalls.join_group_call(message.chat.id, file_path)
         await message.reply_photo(
             photo=f"{AUD_IMG}",
-            caption=f"🏷 **Name:** {title[:50]}\n⏱ **Duration:** `{duration}`\n💡 **Status:** `Playing`\n" \
-                   +f"🎧 **Request by:** {costumer}",
+            caption=f"🏷 **Tên Bài Hát:** {title[:50]}\n⏱ **Thời Lượng:** `{duration}`\n💡 **Trạng Thái:** `Playing`\n" \
+                   +f"🎧 **Khởi Chạy Bởi:** {costumer}",
             reply_markup=keyboard,
         )
         return await lel.delete()
